4f1ad49996ae11c6ec26961fac52dc3e2932b878
andreaamasio <101635283+andreaamasio@users.noreply.github.com>
2025-03-13 12:26:49 +0100

    rendered messages from index.ejs

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,9 +1,21 @@
 const { Router } = require("express")
 //const { getAuthorById } = require("../controllers/indexController")
 const indexRouter = Router()
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+]
 
 indexRouter.get("/", (req, res) => {
-  res.render("index", {})
+  res.render("index", { title: "Mini message board", messages: messages })
 })
 
 module.exports = indexRouter
