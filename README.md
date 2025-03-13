74ca2332b6553e0176d69024ca5d375e3d5f4b66
andreaamasio <101635283+andreaamasio@users.noreply.github.com>
2025-03-13 12:08:18 +0100

    'new' view done

---
+++ README.md
@@ -0,0 +1,1 @@
+Project to revise express
--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const app = express()
 app.set("views", path.join(__dirname, "views"))
 app.set("view engine", "ejs")
 const indexRouter = require("./routes/indexRouter")
+const newRouter = require("./routes/newRouter")
 
+app.use("/new", newRouter)
 app.use("/", indexRouter)
 const PORT = 3000
 app.listen(PORT, () =>
