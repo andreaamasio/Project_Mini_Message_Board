8a28111f38301fd117866cbbc8fb936cd85b973e
andreaamasio <101635283+andreaamasio@users.noreply.github.com>
2025-03-15 06:57:35 +0100

    added style

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const path = require("node:path")
 const app = express()
+const assetsPath = path.join(__dirname, "public")
+app.use(express.static(assetsPath))
 app.set("views", path.join(__dirname, "views"))
 app.set("view engine", "ejs")
 const { indexRouter } = require("./routes/indexRouter")
